eddc31927239a19bc601bda676611099db4fc069
Brock Wilcox <awwaiid@thelackthereof.org>
2015-08-07 12:44:19 -0400

    Save in progress work

--- verify.pl
+++ verify.pl
@@ -1,18 +1,69 @@
 #!/usr/bin/env perl
 
 use v5.20;
-use lib 'local/lib';
+use feature qw(signatures);
+no warnings qw(experimental::signatures);
+
+use lib 'local/lib/perl5';
 use lib 'lib';
 use JSON::MaybeXS;
 use File::Slurp;
 use LCG;
+use Data::Dump;
+
+use Moops;
+
+# Or ... just use [x,y]
+class Cell {
+  has x => (is => 'rw');
+  has y => (is => 'rw');
+}
+
+class Board {
+  has width => (is => 'rw');
+  has height => (is => 'rw');
+  has filled => (is => 'rw');
+
+  method to_array() {
+    my $arr = [];
+    foreach my $x (0..$self->width) {
+      foreach my $y (0..$self->height) {
+        $arr->[$y][$x] = $self->filled->{$x}{$y}
+          ? "F"
+          : "E";
+      }
+    }
+    return $arr;
+  }
+
+  method fill_from_problem($filled) {
+    $self->filled({});
+    foreach my $fill (@$filled) {
+      $self
+        ->filled
+        ->{ $fill->{x} }
+        ->{ $fill->{y} } = 1;
+    }
+  }
+}
 
 my $problem_raw = read_file($ARGV[0]);
 my $problem = decode_json($problem_raw);
 
+my $solution_raw = read_file($ARGV[0]);
+my $solution = decode_json($solution_raw);
+
 say "size: $problem->{height} x $problem->{width}";
 my $source_length = $problem->{sourceLength};
 say "source length: $source_length";
+
+my $board = Board->new(
+  width => $problem->{width},
+  height => $problem->{height}
+);
+$board->fill_from_problem($problem->{filled});
+dd($board);
+say encode_json($board->to_array);
 
 foreach my $seed (@{$problem->{sourceSeeds}}) {
   say "Seed: $seed";
