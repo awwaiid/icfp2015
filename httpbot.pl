3c0b1f168e22671b7b849a14e6e6f5c3391a6dec
Brock Wilcox <awwaiid@thelackthereof.org>
2015-08-07 22:28:25 -0400

    Send json headers for httpbot

--- httpbot.pl
+++ httpbot.pl
@@ -8,11 +8,12 @@ use Continuity;
 $| = 1;
 
 my $server = Continuity->new(
-  debug_level => 2,
+  debug_level => 0,
   port => 8080,
   cookie_session => 0,
   path_session => 0,
   ip_session => 1,
+  no_content_type => 1,
 );
 $server->loop;
 
@@ -20,6 +21,13 @@ sub main {
   my ($request) = @_;
   while(1) {
     my $world = <>; # get the world... and ignore it!
+    $request->print(
+      "Cache-Control: private, no-store, no-cache\r\n",
+      "Pragma: no-cache\r\n",
+      "Expires: 0\r\n",
+      "Content-type: application/json\r\n",
+      "\r\n"
+    );
     $request->print($world);
     my $move = $request->next->param('cmd');
     say $move;
