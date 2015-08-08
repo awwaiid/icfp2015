d2b33c9abc2866f4162a061c1af34f679cab0c41
Brock Wilcox <awwaiid@thelackthereof.org>
2015-08-08 01:14:18 -0400

    Add rotation to keybot

--- keybot.pl
+++ keybot.pl
@@ -15,14 +15,16 @@ sub get_move {
     's' => 'W',
     'x' => 'A',
     'c' => 'F',
-    'f' => 'E'
+    'f' => 'E',
+    'a' => 'P',
+    'g' => 'R'
   }->{$move};
 }
 
 $| = 1;
 while(1) {
   my $world = <>; # get the world... and ignore it!
-  say STDERR "WORLD: $world";
+  # say STDERR "WORLD: $world";
   my $move = get_move();
   say $move;
 }
