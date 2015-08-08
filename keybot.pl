84025dcee6544bd2ecab88fffe6563d22f1c371f
Brock Wilcox <awwaiid@thelackthereof.org>
2015-08-07 23:24:28 -0400

    Fix reset

--- keybot.pl
+++ keybot.pl
@@ -9,6 +9,7 @@ sub get_move {
   open my $tty, '<', '/dev/tty';
   ReadMode 3, $tty;
   my $move = ReadKey(0, $tty);
+  ReadMode 0, $tty;
   chomp $move;
   return {
     's' => 'W',
@@ -21,6 +22,7 @@ sub get_move {
 $| = 1;
 while(1) {
   my $world = <>; # get the world... and ignore it!
+  say STDERR "WORLD: $world";
   my $move = get_move();
   say $move;
 }
