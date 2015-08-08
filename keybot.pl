f0e1c309720ef01ac1cbded2ae5ee412c0e74be6
Brock Wilcox <awwaiid@thelackthereof.org>
2015-08-07 21:07:40 -0400

    add key-driven bot

---
+++ keybot.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/env perl
+
+use v5.18;
+use lib 'local/lib/perl5';
+use lib 'lib';
+use Term::ReadKey;
+
+sub get_move {
+  open my $tty, '<', '/dev/tty';
+  ReadMode 3, $tty;
+  my $move = ReadKey(0, $tty);
+  chomp $move;
+  return {
+    's' => 'W',
+    'x' => 'A',
+    'c' => 'F',
+    'f' => 'E'
+  }->{$move};
+}
+
+$| = 1;
+while(1) {
+  my $world = <>; # get the world... and ignore it!
+  my $move = get_move();
+  say $move;
+}
+
+
+
