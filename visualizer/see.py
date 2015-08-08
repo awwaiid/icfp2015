12052560da721fb59514f2904969f6c7b7ccf7de
Jason Woys <jason@woys.org>
2015-08-07 20:43:39 -0700

    stuff

--- visualizer/see.py
+++ visualizer/see.py
@@ -1,12 +1,14 @@
 import sys
 import json
+import time
 
 my_data = json.loads(open("outputs/problem_1_initial_board.json").read())
+move = 0
 
 x = len(my_data)
 y = len(my_data[0])
 
-print "%d, %d\n" % (x, y)
+#print "%d, %d\n" % (x, y)
 
 def convert(val):
 	if (val == "E"): return "-"
@@ -15,65 +17,68 @@ def convert(val):
 	return "-"
 
 def printSeparator(y):
+	out = ''
 	n = y * 2
 	while n > 0:
   		n -= 1
-  		sys.stdout.write("-"),
-	print ""
+  		out += '-'
+	return out
 
 def printHeader(y):
+	out = ''
 	z = 0
 	n = y
 	while n > 0:
   		n -= 1
-  		print z % 10,
+  		out += str(z % 10) + " "
   		z += 1 
-	print ""
-	return
+	return out
 
 def printMap(map_data):
+	out = ''
 	for i, val in enumerate(map_data):
 		if (i % 2 == 1): 
-			sys.stdout.write(" ")
-		else: 
-			sys.stdout.write("")
+			out += " "
 		for j, row in enumerate(val):
-			sys.stdout.write(convert(row) + " "),
-		print ""	
-
-#printHeader(y)
-#printSeparator(y)
-#printMap(my_data)
-#printSeparator(y)
-
-move = 0
+			out += convert(row) + " "
+	return out
 
 def program_logic(line):
     global move,y,my_data
     move += 1
-    print str(move) + ': ' + line.rstrip()
-    printHeader(y)
-    printSeparator(y)
-    printMap(my_data)
-    printSeparator(y)
-
-    sys.stdout.flush()
+    output = ''
+    output += "Move: " + str(move) + ': ' + line.rstrip()
+    #output += printHeader(y)
+    #output += printSeparator(y)
+    #output += printMap(my_data)
+    #output += printSeparator(y)
+    return output
 
 def read_from_stdin():
-    global line_count
     for line in sys.stdin:
-        program_logic(line)
+        return program_logic(line)
 
 def prompt_user():
-    print 'Type "quit" to exit.'
-    while (True):
-        line = raw_input('PROMPT> ')
-        if line == 'quit':
-            sys.exit()
-        program_logic(line)
+    #line = raw_input('')
+    line = 'test\nfound\nabc'
+#    time.sleep(1)
+#        if line == 'quit':
+#            sys.exit()
+    return program_logic(line)
 
 if __name__ == "__main__":
-    if '-' in sys.argv:
-        read_from_stdin()
-    else:
-        prompt_user()
+	if '-' in sys.argv:
+		out = read_from_stdin()
+		sys.stdout.write(out)
+	else:
+		for i in range(20):
+			#time.sleep(.2)
+			out = prompt_user()
+
+			sys.stdout.write("%s" % out)
+			import subprocess as sp
+			sp.call('clear',shell=True)
+			#sys.stdout.flush()
+			#out = prompt_user()    
+	    #	sys.stdout.write("\r%d" % i)
+	   # 	sys.stdout.flush()
