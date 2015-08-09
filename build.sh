1905b0eedd135bdacf6b3cafe8dee8c4a502d7dd
Brock Wilcox <awwaiid@thelackthereof.org>
2015-08-09 05:09:47 -0400

    Add local cpanm to make it easier

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 
-cpanm -qn -L local Coro::Event
-cpanm -qn --installdeps -L local .
-cpanm -qn -L local Coro::Event
-cpanm -qn --installdeps -L local .
+./cpanm -qn -L local Coro::Event
+./cpanm -qn --installdeps -L local .
+./cpanm -qn -L local Coro::Event
+./cpanm -qn --installdeps -L local .
 
