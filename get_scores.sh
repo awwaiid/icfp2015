227e39a1f05061f585c6cfc06d89515ae8afaee0
Brock Wilcox <awwaiid@thelackthereof.org>
2015-08-09 16:11:07 -0400

    Get the scores from the organizer server

---
+++ get_scores.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+curl --user :YNLCZqJz/CBlN+jALQIxjKpiTd75GfCpwF+iXhjW3nk= -X GET https://davar.icfpcontest.org/teams/72/solutions > scores.json
+
+echo "Wrote scores to scores.json"
+
+
