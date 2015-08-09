507ab32a21a1f6ab2fbdaf715edc0622b9a531ce
Jason Woys <jason@woys.org>
2015-08-09 00:11:49 -0700

    failing bot

---
+++ jbot.php
@@ -0,0 +1,33 @@
+#!/usr/bin/php
+<?php
+
+//$move = '{"map":[["E","E","E","F","E","U","E","E","E","E"],["E","E","E","U","F","E","E","E","E","E"],["E","E","E","F","F","U","E","E","E","E"],["E","E","E","F","E","E","E","E","E","E"],["E","E","F","E","F","E","E","E","E","E"],["E","E","F","F","F","E","E","E","E","E"],["E","E","F","F","E","E","E","E","E","E"],["E","E","F","F","F","F","E","E","E","E"],["E","E","F","F","E","F","F","E","E","E"],["E","F","F","F","F","E","E","E","E","E"]],"moves":["a","l","a","l","l","l","a","a","a","l","a","a","a","a","a","a","l","a","l","a","a","l","l","l","a","l","l","l","l","a","l","a","a","a","a","l","l","l","l","a","a","a","a","a","a","l","l","a","a","l","a","l","l","l","a","l","a","l","a","a","a","a","a","a","l","l","a","a","l"],"status":"Game Over: No room for new unit","source_count":12,"source_length":100,"board":{"width":10,"filled":{"1":{"9":1},"6":{"8":1},"2":{"5":1,"6":1,"8":1,"7":1,"9":1,"4":1},"5":{"7":1,"8":1},"4":{"4":1,"9":1,"7":1,"1":1,"5":1,"2":1},"3":{"0":1,"7":1,"3":1,"9":1,"8":1,"1":1,"2":1,"5":1,"6":1},"9":{},"7":{},"0":{},"8":{}},"height":10,"map":[["E","E","E","F","E","E","E","E","E","E"],["E","E","E","F","F","E","E","E","E","E"],["E","E","E","F","F","E","E","E","E","E"],["E","E","E","F","E","E","E","E","E","E"],["E","E","F","E","F","E","E","E","E","E"],["E","E","F","F","F","E","E","E","E","E"],["E","E","F","F","E","E","E","E","E","E"],["E","E","F","F","F","F","E","E","E","E"],["E","E","F","F","E","F","F","E","E","E"],["E","F","F","F","F","E","E","E","E","E"]]},"current_unit":{"map":[["E","E","F"]],"history":[],"filled":{"2":{"2":1,"0":1},"0":{"1":1},"1":{}},"height":1,"pivot_position":[4,1],"pivot":[1,1],"position":[3,0],"width":3,"orientation":0},"score":25,"units":[{"history":["1","6","1","5","1","9","3","1","1","8","2","4","2","3","3","2","1","7","1","10"],"map":[["F"]],"filled":{"0":{"0":1}},"pivot_position":[1,10],"pivot":[0,0],"height":1,"orientation":0,"position":[1,10],"width":1},{"height":1,"pivot_position":[1,0],"pivot":[1,0],"width":3,"position":[0,0],"orientation":"0","map":[["F","E","F"]],"history":[],"filled":{"0":{"0":1},"1":{},"2":{"0":1}}},{"height":1,"pivot_position":[0,1],"pivot":[0,1],"width":3,"position":[0,0],"orientation":"0","map":[["F","E","E"]],"history":[],"filled":{"2":{},"1":{},"0":{"2":1,"0":1}}},{"filled":{"2":{"2":1,"0":1},"0":{"1":1},"1":{}},"history":[],"map":[["E","E","F"]],"orientation":0,"width":3,"position":[3,0],"pivot":[1,1],"pivot_position":[4,1],"height":1},{"width":3,"position":[3,1],"orientation":0,"height":1,"pivot_position":[4,2],"pivot":[0,1],"filled":{"2":{},"1":{"1":1},"0":{"0":1,"2":1}},"map":[["F","E","E"]],"history":["3","1;3","3;5","2"]},{"pivot":[0,0],"pivot_position":[0,0],"height":1,"orientation":"0","width":2,"position":[0,0],"history":[],"map":[["F","F"]],"filled":{"0":{"0":1},"1":{"0":1}}},{"position":[3,6],"width":2,"orientation":0,"height":1,"pivot":[1,0],"pivot_position":[4,6],"filled":{"0":{"0":1},"1":{"0":1}},"map":[["F","F"]],"history":["4","4;5","4","3","2;4","2","3","5;4","5","3","3;4","3","3","1;4","1","3","6;4","6"]},{"height":1,"pivot":[0,0],"pivot_position":[3,4],"position":[3,4],"width":2,"orientation":0,"map":[["F","E"]],"history":["3","4;3","5","3","3;4","4","4","2;4","3","4","1;5","2"],"filled":{"1":{},"0":{"1":1,"0":1}}},{"filled":{"1":{},"0":{"0":1,"1":1}},"map":[["F","E"]],"history":["3","1;4","2","3","2;3","3"],"position":[3,2],"width":2,"orientation":0,"height":1,"pivot_position":[3,3],"pivot":[0,1]},{"map":[["F","F","F"]],"history":[],"filled":{"1":{"0":1},"0":{"0":1},"2":{"0":1}},"height":1,"pivot_position":[0,0],"pivot":[0,0],"width":3,"position":[0,0],"orientation":"0"},{"history":[],"map":[["F","F","F"]],"filled":{"2":{"0":1},"1":{"0":1},"0":{"0":1}},"pivot":[1,0],"pivot_position":[1,0],"height":1,"orientation":"0","width":3,"position":[0,0]},{"map":[["F","F","F"]],"history":[],"filled":{"2":{"0":1},"0":{"0":1},"1":{"0":1}},"height":1,"pivot_position":[2,0],"pivot":[2,0],"width":3,"position":[0,0],"orientation":"0"},{"pivot_position":[5,8],"pivot":[0,0],"height":1,"orientation":0,"position":[5,8],"width":3,"history":["4","4;4","5;4","6","3","5;3","7;4","6","2","1;2","3;3","2","4","7;4","9;5","8","3","3;3","5;4","4","5","10;5","8;5","9","3","2;3","3;3","4","4","6;4","7;4","8"],"map":[["F","E","E"]],"filled":{"2":{},"0":{"0":1,"1":1,"2":1},"1":{}}},{"history":["2","2;2","3;2","4","1","3;1","5;2","4","2","4;2","5;2","6","2","1;2","3;3","2","2","5;2","7;3","6"],"map":[["F","E","E"]],"filled":{"0":{"0":1,"1":1,"2":1},"1":{},"2":{}},"pivot":[0,1],"pivot_position":[3,6],"height":1,"orientation":0,"position":[2,5],"width":3},{"filled":{"0":{"2":1,"1":1,"0":1},"1":{},"2":{}},"map":[["F","E","E"]],"history":[],"width":3,"position":[0,0],"orientation":"0","height":1,"pivot":[0,2],"pivot_position":[0,2]},{"height":1,"pivot_position":[1,0],"pivot":[1,0],"position":[0,0],"width":3,"orientation":"0","map":[["E","F","E"]],"history":[],"filled":{"2":{},"0":{"1":1},"1":{"2":1,"0":1}}},{"width":3,"position":[2,8],"orientation":0,"height":1,"pivot":[1,1],"pivot_position":[3,9],"filled":{"1":{"2":1,"0":1},"0":{"1":1},"2":{}},"map":[["E","F","E"]],"history":["2","7;2","8;2","9","3","5;4","4;4","6","2","7;3","6;3","8","2","9;3","10;3","8","4","1;4","2;4","3","4","3;4","4;4","5","3","3;4","2;4","4","3","5;3","6;3","7"]},{"height":1,"pivot":[1,2],"pivot_position":[1,2],"position":[0,0],"width":3,"orientation":"0","map":[["E","F","E"]],"history":[],"filled":{"2":{},"0":{"1":1},"1":{"0":1,"2":1}}}]}';
+
+while ($line = trim(fgets(STDIN))) {
+	$myArray = json_decode($line, true);
+
+	var_dump (json_last_error());
+	var_dump ($line);
+
+	var_dump ($myArray);
+
+	flog(print_r($myArray['moves'], true));
+	//var_dump ($line);
+	
+	$move = getMove($myArray['moves']);
+	flog($move);
+
+	print $move . "\n";
+}
+
+
+
+function flog ($message) {
+	error_log($message . "\n", 3, "/tmp/phperror");
+}
+
+function getMove($moves) {
+	return array_pop($moves) == 'l' ? 'a' : 'l';
+	//return 'l';
+}
+
