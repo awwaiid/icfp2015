d975d0d0d53c487020e0f7774ff2481588bdcba6
Jason Woys <jason@woys.org>
2015-08-09 15:01:45 -0700

    bot stuff

--- jbot.php
+++ jbot.php
@@ -2,67 +2,60 @@
 <?php
 flog("Starting");
 while ($line = trim(fgets(STDIN))) {
-	flog("Line Length: " . strlen($line));
-	$myArray = json_decode($line, true);
-	flog($myArray['status']);
-	
-	if (!$myArray) {
-		die("Bad Json " . json_last_error() . "\n");
-	}
-	//flog(print_r($myArray['moves'], true));
+    flog("Line Length: " . strlen($line));
+    $myArray = json_decode($line, true);
+    flog($myArray['status']);
+    
+    if (!$myArray) {
+        die("Bad Json " . json_last_error() . "\n");
+    }
+    //flog(print_r($myArray['moves'], true));
 
-	$move = getMove($myArray);
+    $move = getMove($myArray);
 
 
-	flog (count($myArray['moves']));
-	flog($move);
-	
-	if (strpos($myArray['status'], 'Game Over') !== false) {
-		die('Game over');
-	}
-	print $move . "\n";
+    flog (count($myArray['moves']));
+    flog($move);
+    
+    if (strpos($myArray['status'], 'Game Over') !== false) {
+        die('Game over');
+    }
+    print $move . "\n";
 }
 flog("ended");
 
 function flog ($message) {
-	error_log($message . "\n", 3, "/tmp/phperror");
+    //error_log($message . "\n", 3, "/tmp/phperror");
 }
 
 function getMove($world) {
-	$moves= $world['moves'];
-	$validMoves = $world['valid_moves'];
+    //$moves = $world['moves'];
+    $validMoves = $world['valid_moves'];
 
-	if (in_array('b', $validMoves)) {
-		return 'e';
-	}
+    if (in_array('b', $validMoves)) {
+        return 'e';
+    }
 
-	if (in_array('a', $validMoves)) {
-		return 'i';
-	}
+    if (in_array('a', $validMoves)) {
+        return 'i';
+    }
 
-	if (in_array('l', $validMoves)) {
-		return 'l';
-	}
+    if (in_array('l', $validMoves)) {
+        return 'l';
+    }
 
-	if (in_array('p', $validMoves)) {
-		return '!';
-	}
+    if (in_array('p', $validMoves)) {
+        return '!';
+    }
 
-	if (in_array('d', $validMoves)) {
-		return 'd';
-	}
-	if (in_array('k', $validMoves)) {
-		return 'k';
-	}
+    if (in_array('d', $validMoves)) {
+        return 'd';
+    }
+    if (in_array('k', $validMoves)) {
+        return 'k';
+    }
 
-	return ' ';
+    return ' ';
 
-
-//	$currentPos = $myArray['current_unit']['position'];
-//	return array_pop($moves) == 'l' ? 'a' : 'l';
 }
-
-//function checkAdjacent(curx, cury) {
-//}
-
 
