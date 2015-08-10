11551273129b4a1e60979d703fadb3adb85cac71
Erik Summerfield <esummerfield@Eriks-MacBook-Pro.local>
2015-08-09 21:25:22 -0500

    add non working verison of worldplay.pl

---
+++ worldplay.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+use v5.18;
+use World;
+
+my $worlddata = "@ARGV"; #get our world object
+chomp $worlddata;
+last if ! $worlddata;
+$worlddata = decode_json($worlddata);
+my $world = World->new(
+  problem_id    => $worlddata->problem_id   ,
+  version_tag   => $worlddata->version_tag  ,
+  seed          => $worlddata->seed         ,
+  power_phrases => $worlddata->power_phrases,
+  board         => $worlddata->board        ,
+  units         => $worlddata->units        ,
+  source_count  => $worlddata->source_count ,
+  source_length => $worlddata->source_length,
+);
+
+foreach my $move (@{$worlddata->{moves}}) {
+  $world->move($move);
+}
+while(1) {
+  my $move = <>; # get the world... and ignore it!
+  $world->move($move);
+  say $world->to_json();
+}
