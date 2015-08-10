fa7b3e14eae1a36ee22525cbb706019111618abd
Jason Woys <jason@woys.org>
2015-08-09 19:19:44 -0700

    stuff

--- jbot.php
+++ jbot.php
@@ -1,31 +1,35 @@
 #!/usr/bin/php
 <?php
-flog("Starting");
 while ($line = trim(fgets(STDIN))) {
-    flog("Line Length: " . strlen($line));
-    $myArray = json_decode($line, true);
-    flog($myArray['status']);
-    
-    if (!$myArray) {
+    $world = json_decode($line, true);
+    if (!$world || !is_array($world)) {
         die("Bad Json " . json_last_error() . "\n");
     }
-    //flog(print_r($myArray['moves'], true));
-
-    $move = getMove($myArray);
-
-
-    flog (count($myArray['moves']));
-    flog($move);
-    
-    if (strpos($myArray['status'], 'Game Over') !== false) {
-        die('Game over');
+    $move = getMove($world);
+    if (strpos($world['status'], 'Game Over') !== false) {
+        exit;
     }
+    move($move);
+}
+function flog ($message) {
+    if (is_array($message)) {
+        $message = print_r($message, true);
+    }
+    error_log($message . "\n", 3, "/tmp/phperror");
+}
+
+function move($move) {
     print $move . "\n";
 }
-flog("ended");
 
-function flog ($message) {
-    //error_log($message . "\n", 3, "/tmp/phperror");
+function rotate($world) {
+    if (in_array('d', $world['valid_moves'])) {
+        return 'd';
+    }
+    if (in_array('k', $world['valid_moves'])) {
+        return 'k';
+    }
+    else return false;
 }
 
 function getMove($world) {
@@ -47,15 +51,12 @@ function getMove($world) {
     $totalMoveCount = count($world['moves']);
 
 
-    if ($totalMoveCount % 3 == 0 && !isBadTriangle($world)) {
-        if (in_array('d', $validMoves)) {
-            return 'k';
-        }
-        if (in_array('k', $validMoves)) {
-            return 'd';
-        }
+    if ($totalMoveCount % 3 == 0 && !isBadTriangle($world) && $rotate = rotate($world)) {
+        return $rotate;
     }
-    if ($world['source_count'] % 2 == 0) {
+    $eastOrWest = eastOrWest($world);
+    flog ($eastOrWest);
+    if ($eastOrWest == 'E') {
         // go east
         if (in_array('b', $validMoves)) {
             return 'e';
@@ -98,23 +99,40 @@ function getMove($world) {
     return count($world['legal_moves'])
         ? $world['legal_moves'][0]
         : exit;
-
-
-
 }
 
-function lowestOpenSpot($world) {
-    
+function eastOrWest($world) {
+    $firstFilled = [];
+    $map = $world['board']['map'];
+    foreach ($map as $row => $m) {
+        foreach ($m as $col => $l) {
+            if ($l == 'F') {
+                $firstFilled = [$row, $col];
+            }
+        }
+    }
+    $pos = $world['current_unit']['position'];
+    flog ($pos);
+    flog ($firstFilled);
+    if ($pos[1] < $firstFilled[1]) {
+        return 'E';
+    } else if ($pos[1] > $firstFilled[1]) {
+        return 'W';
+    } else {
+        return 'S';
+    }
+
 }
 
 function isBadTriangle($world) {
+
     $currentUnit = $world['current_unit'];
     $filled = $currentUnit['filled'];
-    if (count($filled[0]) + count($filled[1]) == 3) {
-        //if ($currentUnit['orientation'] > 2) {
-            return true;
-        //}
+    $count = 0;
+    foreach ($filled as $f) {
+        $count += $f[0];
     }
+    return $count == 3;
 }
 
 
